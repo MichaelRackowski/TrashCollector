9f16bb044239708a4f713f5554d01fbef223ff3f
Michael Rackowski <MichaelRackowski@gmail.com>
2019-10-02 15:44:14 -0500

    Added GetUserId method to the employee and customer controller to hold the applicationId

--- TrashCollectorfr/Controllers/CustomersController.cs
+++ TrashCollectorfr/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -48,6 +49,7 @@ namespace TrashCollectorfr.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,FirstName,LastName,StreetName,City,State,Zipcode")] Customer customer)
         {
+            customer.ApplicationId = User.Identity.GetUserId();
             if (ModelState.IsValid)
             {
                 db.Customers.Add(customer);
