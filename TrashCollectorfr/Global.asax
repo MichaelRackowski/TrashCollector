92cb254ef157365a70ec353353bb38aac5ccbd14
Michael Rackowski <MichaelRackowski@gmail.com>
2019-10-02 11:26:12 -0500

    Init

---
+++ TrashCollectorfr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrashCollectorfr.MvcApplication" Language="C#" %>
---
+++ TrashCollectorfr/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using TrashCollectorfr.Models;
+
+[assembly: OwinStartupAttribute(typeof(TrashCollectorfr.Startup))]
+namespace TrashCollectorfr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "michael";
+                user.Email = "MichaelRackowski@gmail.com";
+
+                string userPWD = "Buddyman101";
+                var chkUser = UserManager.Create(user, userPWD);
+
+                if(chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+                }
+
+            }
+
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+            }
+
+            if(!roleManager.RoleExists("Employee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
+        }
+    }
+}
